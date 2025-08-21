0e0647f30a90c3ceb86f364e531dddc0f10c63b7
Zeeshan <mzeeshi187@gmail.com>
2025-08-21 09:06:57 -0700

    home setting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,9 +11,11 @@
     />
     <div class="flex items-end min-h-[100vh] rounded-br-[30px] rounded-bl-[30px] overflow-hidden">
       <div
-        class="flex rounded-br-[30px] rounded-bl-[30px] justify-center xl:!justify-between flex-col xl:!flex-row items-center gap-[20px] px-5 2xl:!px-[50px] py-[30px] bg-transparent min-h-[50%] gradient_bgc z-40 w-full"
+        class=" rounded-br-[30px] rounded-bl-[30px]  px-5 2xl:!px-[50px] py-[30px] bg-transparent min-h-[50%] gradient_bgc z-40 w-full"
       >
-        <div class="flex flex-col gap-5 items-center xl:!items-start w-full xl:!w-[40%]">
+      <!-- main section  -->
+        <div class="flex justify-center xl:!justify-between flex-col xl:!flex-row items-center gap-[20px] mx-[30px] mb-[50px]">
+          <div class="flex flex-col gap-5 items-center xl:!items-start w-full xl:!w-[40%]">
           <img src="@/assets/logo.png" alt="" class="w-[300px] xl:!w-[50%]" />
           <div
             class="flex items-center justify-center xl:!justify-start gap-[20px] sm:!flex-row w-full flex-col"
@@ -26,9 +28,6 @@
             <img src="@/assets/profile_button.svg" alt="" class="w-[90px] xl:!w-[20%]" />
             <i class="fa-solid fa-gear text-white text-[30px]"></i>
           </div>
-          <p class="text-base 2xl:!text-xl poppins text-[#FFFFFF] underline opacity-[50%]">
-            Contacter le Centre d’aide
-          </p>
         </div>
         <div
           class="flex items-center justify-center xl:justify-end gap-[50px] w-full xl:!w-[40%] flex-col sm:!flex-row"
@@ -36,6 +35,10 @@
           <img src="@/assets/img3.png" alt="" class="w-[300px] xl:!w-[70%] rounded-[20px]" />
           <img src="@/assets/img2.svg" alt="" class="w-[110px] xl:!w-[20%]" />
         </div>
+        </div>
+        <p class="text-base 2xl:!text-xl poppins text-[#FFFFFF] underline opacity-[50%] mx-[30px] absolute bottom-[30px] left-[50px]">
+            Contacter le Centre d’aide
+          </p>
       </div>
     </div>
   </div>
